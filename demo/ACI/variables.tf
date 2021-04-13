a82fcfcc160efc34689264709999609ecd5cf3a1
scotttyso <tyscott@cisco.com>
2021-04-13 17:42:14 -0600

    changing back to agent

--- demo/ACI/variables.tf
+++ demo/ACI/variables.tf
@@ -1,14 +1,16 @@
 variable "apicUser" {
-  type        = string
-  description = "APIC Username"
+  type          = string
+  description   = "APIC Username"
+  default       = "admin"
 }
 
 variable "apicPass" {
-  type        = string
-  description = "APIC Password"
+  type          = string
+  description   = "APIC Password"
 }
 
 variable "apicHost" {
-  type        = string
-  description = "API Endpoint URL"
+  type          = string
+  description   = "API Endpoint URL"
+  default       = "https://wakanda-apic01.rich.ciscolabs.com"
 }
