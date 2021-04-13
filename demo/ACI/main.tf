faa393dead389bb4e406328c39cf3cd6350cd81e
scotttyso <tyscott@cisco.com>
2021-04-13 16:34:59 -0600

    added dependencies

--- demo/ACI/main.tf
+++ demo/ACI/main.tf
@@ -12,33 +12,42 @@ resource "aci_tenant" "tenant" {
 }
 
 resource "aci_application_profile" "ap" {
+  depends_on  = [aci_tenant.tenant]
   tenant_dn   = aci_tenant.tenant.id
   name        = "k8s"
   description = "Created with Terraform"
 }
 
 resource "aci_bridge_domain" "bridge_domain" {
-        tenant_dn                   = aci_tenant.tenant.id
-        description                 = "sample bridge domain"
-        name                        = "iks_bd"
-        relation_fv_rs_ctx = "uni/tn-common/ctx-wakanda"
-        relation_fv_rs_bd_to_out = ["uni/tn-common/out-wakandaL3Out"]
-
+  depends_on                = [aci_tenant.tenant]
+  tenant_dn                 = aci_tenant.tenant.id
+  description               = "sample bridge domain"
+  name                      = "iks_bd"
+  relation_fv_rs_ctx        = "uni/tn-common/ctx-wakanda"
+  relation_fv_rs_bd_to_out  = ["uni/tn-common/out-wakandaL3Out"]
 }
 
 resource "aci_subnet" "subnet" {
-  parent_dn = aci_bridge_domain.bridge_domain.id
-  ip = "10.139.191.1/24"
-  preferred = "yes"
-  scope = ["public"]
+  depends_on    = [
+    aci_tenant.tenant,
+    aci_bridge_domain.bridge_domain
+  ]
+  parent_dn     = aci_bridge_domain.bridge_domain.id
+  ip            = "10.139.191.1/24"
+  preferred     = "yes"
+  scope         = ["public"]
 }
 
 resource "aci_application_epg" "epg" {
-    application_profile_dn  = aci_application_profile.ap.id
-    name                    = "iks"
-    relation_fv_rs_bd       = aci_bridge_domain.bridge_domain.id
-    relation_fv_rs_prov = ["uni/tn-common/brc-default"]
-    relation_fv_rs_cons = ["uni/tn-common/brc-default"]
+  depends_on                = [
+    aci_bridge_domain.bridge_domain,
+    aci_application_profile.ap
+  ]
+  application_profile_dn    = aci_application_profile.ap.id
+  name                      = "iks"
+  relation_fv_rs_bd         = aci_bridge_domain.bridge_domain.id
+  relation_fv_rs_prov       = ["uni/tn-common/brc-default"]
+  relation_fv_rs_cons       = ["uni/tn-common/brc-default"]
 }
 
 resource "aci_epg_to_domain" "iks" {
