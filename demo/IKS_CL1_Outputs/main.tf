7692dc9d27281a357a6783b0af79eaf1c30c9d16
scotttyso <tyscott@cisco.com>
2021-06-09 12:19:30 -0400

    uploading changes to main.tf

--- demo/IKS_CL1_Outputs/main.tf
+++ demo/IKS_CL1_Outputs/main.tf
@@ -22,6 +22,10 @@ data "intersight_kubernetes_cluster" "workshop" {
     name = "cloud-iac-cl1"
 }
 
+output "kube_config" {
+    value = base64decode(data.intersight_kubernetes_cluster.workshop.results[0].kube_config)
+}
+
 output "url" {
     value = local.kube_config.clusters[0].cluster.server
 }
@@ -30,14 +34,14 @@ output "hostname" {
     value = trimsuffix(trimprefix(local.kube_config.clusters[0].cluster.server, "https://"), regex("[:]\\d+", trimprefix(local.kube_config.clusters[0].cluster.server, "https://")))
 }
 
-output "cluster_ca_certificate" {
+output "certificate-authority-data" {
     value = local.kube_config.clusters[0].cluster.certificate-authority-data
 }
 
-output "client_certificate" {
+output "client-certificate-data" {
     value = local.kube_config.users[0].user.client-certificate-data
 }
 
-output "client_key" {
+output "client-key-data" {
     value = local.kube_config.users[0].user.client-key-data
 }
