a5516a5491929a3ecea1b6de6dfb4153cd44d4d3
scotttyso <tyscott@cisco.com>
2022-01-10 16:28:36 -0500

    uploading formating updates

--- ACI/templates/Access_Policies/vpc_descr.tf
+++ ACI/templates/Access_Policies/vpc_descr.tf
@@ -7,9 +7,9 @@ GUI Location:
  - Fabric > Access Policies > Policies > Switch > Virtual Port Channel default
 */
 resource "aci_rest" "vpc_description" {
-    path        = "/api/node/mo/uni/fabric/protpol.json"
-    class_name    = "fabricProtPol"
-    payload        = <<EOF
+  path       = "/api/node/mo/uni/fabric/protpol.json"
+  class_name = "fabricProtPol"
+  payload    = <<EOF
 {
     "fabricProtPol": {
         "attributes": {
